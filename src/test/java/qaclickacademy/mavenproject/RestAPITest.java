00677ccfe6ae432d5627e30863d99c88ef638585
smriti21204 <smriti.solegaonkar@gmail.com>
2022-12-14 22:54:52 +0530

    RestAPI section modified

--- src/test/java/qaclickacademy/mavenproject/RestAPITest.java
+++ src/test/java/qaclickacademy/mavenproject/RestAPITest.java
@@ -7,6 +7,9 @@ public class RestAPITest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("changes1");
+		System.out.println("changes2");
+		System.out.println("changes3");
 	}
 
 	@Test
